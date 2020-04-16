073bc9f15daa14fae44a3700b8de2fa4a878c47d
Sean Baier <sean.baier@nrccua.org>
2020-04-15 21:10:19 -0500

    linting

--- src/seed/config/mongodb.js
+++ src/seed/config/mongodb.js
@@ -1,9 +1,12 @@
-const mongodb = require('mongodb');
-require('dotenv').config();
+const mongodb = require("mongodb");
+require("dotenv").config();
 
 module.exports = async function connect() {
   const options = { useNewUrlParser: true, useUnifiedTopology: true };
-  const client = await mongodb.MongoClient.connect(process.env.MONGODB_URI, options);
+  const client = await mongodb.MongoClient.connect(
+    process.env.MONGODB_URI,
+    options
+  );
   const db = client.db(process.env.PROJECT_NAME);
   db.close = client.close;
 
