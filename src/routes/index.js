23cfa6ee0e789312d6fcca04723338a2c85d8b13
Sean Baier <sean.t.baier@gmail.com>
2020-10-17 05:14:04 -0500

    test 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const router = express.Router()
 router.get('/', (req, res) => {
-  res.send({ message: 'hello' })
+  res.send({ message: 'hello deployed through cicd' })
 })
 
 router.post('/', (req, res) => {
